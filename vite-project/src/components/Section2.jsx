3c2b084795155902145c022761f79867b4f1fcab
Tushar <tushar.abc15@gmail.com>
2026-03-05 17:01:46 +0000

    Fix self-closing br tag in Section2 JSX

--- vite-project/src/components/Section2.jsx
+++ vite-project/src/components/Section2.jsx
@@ -27,8 +27,8 @@ const Section2 = () => {
         <p className="section2-text">
           I still can’t believe we randomly crossed paths on Hinge on 01 February 2026. I mean… what are the odds? I sent the like randomly around 16 December 2025 with zero expectations… and then you actually matched. I’m not saying it was fate, but Hinge algorithm deserves a thank you card for actually bringing my profile to your feed.
           And now here we are… talking, laughing, sharing random thoughts like we’ve known each other longer than we actually have and how can we forget you calling me 'Cringe' whenever I share a random reel in my IG feed with you (which means that IG wants us to tease each other, more! :P) Not bad for two strangers who met on Hinge randomly. 
-          And someone was too busy that they responded to my text after 2 days of being matched, i.e. on 3rd February 2026, I still remember the day! :( <br>
-          But woh kehte hain naa, "If it’s meant for you, it will find its way to you". 
+          And someone was too busy that they responded to my text after 2 days of being matched, i.e. on 3rd February 2026, I still remember the day! :( <br />
+          But woh kehte hain naa, "If it's meant for you, it will find its way to you".
           Also, Do you even remember our first call? Nahi naa, chalo, i tell you, it was 26/2/26, 20:44.
           So I was thinking… instead of just being “that yet another guy from Hinge,” how about I ask you something in a different way.
         </p>
